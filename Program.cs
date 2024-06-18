752bc27770f727e282699fe439dbe180cad9a660
Paulo <paulforthhe@gmail.com>
2024-06-18 18:48:45 -0300

    Primeiro commmit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Projeto_Web_Lh_Pets_versão_1;
+
+namespace Projeto_Web_PH;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Projeto Web PH Pets - V.1!");
+        
+        app.UseStaticFiles();
+        app.MapGet("/index", (HttpContext contexto)=> {
+            contexto.Response.Redirect("index.html",false);
+        });
+        Banco dba=new Banco();
+        app.MapGet("/listaClientes", (HttpContext contexto) =>{
+        contexto.Response.WriteAsync( dba.GetListaString());
+    });
+        
+
+        app.Run();
+    }
+}
+
